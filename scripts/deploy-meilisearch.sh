532a2b88a7a85f610e13519c576025774cbf48ae
Samuel Jimenez <sjimenezre@gmail.com>
2021-01-06 19:00:31 +0100

    Install MeiliSearch from cloud-init

--- scripts/deploy-meilisearch.sh
+++ scripts/deploy-meilisearch.sh
@@ -1,12 +1,5 @@
 export DEBIAN_FRONTEND=noninteractive
 
-# Install build dependencies
-echo "deb http://ftp.de.debian.org/debian sid main" >> /etc/apt/sources.list
-
-# Install MeiliSearch v0.17.0
-wget --directory-prefix=/etc/meilisearch/ https://github.com/meilisearch/MeiliSearch/releases/download/v0.17.0/meilisearch.deb
-apt install /etc/meilisearch/meilisearch.deb
-
 # Prepare systemd service for MeiliSearch
 cat << EOF >/etc/systemd/system/meilisearch.service
 [Unit]
