48fb6251be4221ea4a018f1461ec74f5ac587780
Samuel Jimenez <sjimenezre@gmail.com>
2021-01-11 17:20:25 +0100

    Update deploy-meilisearch.sh and cloud-config.yaml

--- scripts/deploy-meilisearch.sh
+++ scripts/deploy-meilisearch.sh
@@ -1,45 +1,4 @@
-export DEBIAN_FRONTEND=noninteractive
-
-# Prepare systemd service for MeiliSearch
-cat << EOF >/etc/systemd/system/meilisearch.service
-[Unit]
-Description=MeiliSearch
-After=systemd-user-sessions.service
-
-[Service]
-Type=simple
-ExecStart=/usr/bin/meilisearch --db-path /var/lib/meilisearch/data.ms
-Environment="MEILI_SERVER_PROVIDER=digital_ocean"
-
-[Install]
-WantedBy=default.target
-EOF
-
-# Start MeiliSearch service
-systemctl enable meilisearch
-systemctl start meilisearch
-
-# Delete default Nginx config
-rm /etc/nginx/sites-enabled/default
-
-# Set Nginx to proxy MeiliSearch
-cat << EOF > /etc/nginx/sites-enabled/meilisearch
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    server_name _;
-
-    location / {
-        proxy_pass  http://127.0.0.1:7700;
-    }
-}
-EOF
-systemctl restart nginx
-
 # Copy MeiliSearch configuration scripts
-mkdir -p /var/log/meilisearch
-mkdir -p /var/lib/meilisearch
 mkdir -p /var/opt/meilisearch/scripts/first-login
 git clone https://github.com/meilisearch/meilisearch-digital-ocean.git /tmp/meili-tmp
 cd /tmp/meili-tmp
@@ -50,6 +9,7 @@ chmod 755 /tmp/meili-tmp/scripts/MOTD/*
 cp -r /tmp/meili-tmp/scripts/per-instance/* /var/lib/cloud/scripts/per-instance/.
 cp -r /tmp/meili-tmp/scripts/first-login/* /var/opt/meilisearch/scripts/first-login/.
 cp -r /tmp/meili-tmp/scripts/MOTD/* /etc/update-motd.d/.
+rm -rf /tmp/meili-tmp
 
 # Set launch MeiliSearch first login script
 touch /var/opt/meilisearch/env
