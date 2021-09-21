c4e450e61429c62c5a197560bc5ed69a2317cddf
cvermand <33010418+bidoubiwa@users.noreply.github.com>
2021-09-21 10:58:20 +0200

    Update scripts/version updates/update_meilisearch_version.sh
    Co-authored-by: Amélie <alallema@users.noreply.github.com>

--- scripts/version updates/update_meilisearch_version.sh
+++ scripts/version updates/update_meilisearch_version.sh
@@ -7,20 +7,11 @@ meilisearch_version=$1
 
 ## Constants
 
-RED="\033[0;31m"
 BRED="\033[1;31m"
-GREEN="\033[0;32m"
 BGREEN="\033[1;32m"
-YELLOW="\033[0;33m"
 BYELLOW="\033[1;33m"
-BLUE="\033[0;34m"
 BBLUE="\033[1;34m"
-PINK="\033[0;35m"
 BPINK="\033[1;35m"
-CYAN="\033[0;36m"
-BCYAN="\033[1;36m"
-GREY="\033[0;37m"
-BGREY="\033[1;37m"
 WHITE="\033[0m"
 NC="\033[0m"
 ERROR_LABEL="${BRED}error: ${NC}"
