b798565f11682e516924cf275a36cf38b3ff8137
viogustian <viognuralamsyah@gmail.com>
2021-10-04 22:33:38 +0700

    change quotes to apostrophes

--- scripts/deploy-meilisearch.sh
+++ scripts/deploy-meilisearch.sh
@@ -4,16 +4,16 @@ mkdir /var/opt/meilisearch/dumps
 git clone https://github.com/meilisearch/cloud-scripts.git /tmp/meili-tmp
 cd /tmp/meili-tmp # DELETE?
 
-git checkout "$1"
+git checkout '$1'
 chmod 755 /tmp/meili-tmp/scripts/first-login/*
 chmod 755 /tmp/meili-tmp/scripts/MOTD/*
 cp -r /tmp/meili-tmp/scripts/first-login/* /var/opt/meilisearch/scripts/first-login/.
-sed -i "s/provider_name/$2/" /var/opt/meilisearch/scripts/first-login/000-set-meili-env.sh
+sed -i 's/provider_name/$2/' /var/opt/meilisearch/scripts/first-login/000-set-meili-env.sh
 cp -r /tmp/meili-tmp/scripts/MOTD/* /etc/update-motd.d/.
 rm -rf /tmp/meili-tmp
 
 # Set launch MeiliSearch first login script
 touch /var/opt/meilisearch/env
-echo "source /var/opt/meilisearch/env" >> /root/.bashrc
-echo "source /var/opt/meilisearch/env" >> /home/meilisearch/.bashrc
-echo "source /var/opt/meilisearch/env" >> /etc/skel/.bashrc
+echo 'source /var/opt/meilisearch/env' >> /root/.bashrc
+echo 'source /var/opt/meilisearch/env' >> /home/meilisearch/.bashrc
+echo 'source /var/opt/meilisearch/env' >> /etc/skel/.bashrc
