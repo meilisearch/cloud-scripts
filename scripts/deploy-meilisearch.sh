4b3328330ad45db0147a1a67c24d0af5139400f0
viogustian <80340260+viogustian@users.noreply.github.com>
2021-10-05 14:58:14 +0700

    Update scripts/deploy-meilisearch.sh
    Co-authored-by: Amélie <alallema@users.noreply.github.com>

--- scripts/deploy-meilisearch.sh
+++ scripts/deploy-meilisearch.sh
@@ -4,7 +4,7 @@ mkdir /var/opt/meilisearch/dumps
 git clone https://github.com/meilisearch/cloud-scripts.git /tmp/meili-tmp
 cd /tmp/meili-tmp # DELETE?
 
-git checkout '$1'
+git checkout "$1"
 chmod 755 /tmp/meili-tmp/scripts/first-login/*
 chmod 755 /tmp/meili-tmp/scripts/MOTD/*
 cp -r /tmp/meili-tmp/scripts/first-login/* /var/opt/meilisearch/scripts/first-login/.
